b97a7375a178e0c3baccf820beb2fda1b03d7460
evil-tim <10543339+evil-tim@users.noreply.github.com>
2026-08-30 16:47:11 +0800

    feat: rework contract address config source in Chainlink and Uniswap data sources

--- price_sources/chainlink_datasource.py
+++ price_sources/chainlink_datasource.py
@@ -6,6 +6,7 @@ utility functions to obtain the price at the current time.
 """
 
 from decimal import Decimal
+from typing import cast
 
 from web3 import Web3
 
@@ -14,15 +15,29 @@ from price_sources.web3_datasource import Web3DataSource
 
 
 class ChainlinkDataSource(Web3DataSource):
+
+    def __init__(self, config: dict):
+        super().__init__(config)
+
+        if not self.config.get("contract_address"):
+            raise ValueError("config must include 'contract_address'")
+        self.contract_address: str = cast(str, self.config.get("contract_address"))
+
+        self.invert = bool(self.config.get("invert"))
+
     def fetch_blockchain_price(
         self,
-        contract_address: str,
         web3: Web3,
     ) -> Decimal:
         """Fetch the price of a token from Chainlink using the provided Web3 instance.
         Using the price feed contract address get the latest round data.
         """
 
-        return get_chainlink_price_feed_price(
-            contract_address=contract_address, web3=web3
+        result = get_chainlink_price_feed_price(
+            contract_address=self.contract_address, web3=web3
         )
+
+        if self.invert:
+            return 1 / result
+        else:
+            return result
